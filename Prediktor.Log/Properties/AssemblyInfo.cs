b15f0718cf67d54625eaafb78bb87c9e578b4a8c
Christian Fjeld <christian_fjeld@hotmail.com>
2021-08-24 09:12:48 +0200

    version update

--- Prediktor.Log/Properties/AssemblyInfo.cs
+++ Prediktor.Log/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Prediktor")]
 [assembly: AssemblyCopyright("© Prediktor AS. All rights reserved")]
 [assembly: AssemblyTrademark("APIS")]
-[assembly: AssemblyVersion("1.0.7")]
+[assembly: AssemblyVersion("1.0.8")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
